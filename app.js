26c634d04d244b2a0069e3d30e2747e98a135a08
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-14 10:29:53 -0600

    Commit 5 - Sum Array.

--- app.js
+++ app.js
@@ -3,39 +3,49 @@
 // the function can be used in an expression or inside of other functions
 
 
-const checkPalindrome = (str) => {
-    const arr = str.split('');
-    const reverseArr = arr.reverse();
-    const reverseStr = reverseArr.join('');
-    if (str === reverseStr){
-        console.log(`${str} is a palindrome`);
-        return true;
-    } else {
-        console.log(`${str} is not a palindrome`);
-        return false;
+// const checkPalindrome = (str) => {
+//     const arr = str.split('');
+//     const reverseArr = arr.reverse();
+//     const reverseStr = reverseArr.join('');
+//     if (str === reverseStr){
+//         console.log(`${str} is a palindrome`);
+//         return true;
+//     } else {
+//         console.log(`${str} is not a palindrome`);
+//         return false;
+//     }
+// }
+
+// checkPalindrome('racecar');
+// checkPalindrome('hello');
+
+// const sumDigits = (num) => {
+//     const numString = String(num);
+//     const digits = numString.split('');
+//     let total = 0;
+//     for (let i = 0; i < digits.length; i++){
+//         total +=(Number(digits[i]));
+//     }
+//     return total;
+// }
+
+// console.log(sumDigits(42));
+
+// const calculateSide = (sideA, sideB) => {
+//     const cSquared = Math.pow(sideA, 2) + Math.pow(sideB, 2);
+//     const sideC = Math.sqrt(cSquared);
+//     return sideC;
+// }
+// console.log(calculateSide(3,4));
+
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
     }
+    return sum;
 }
 
-checkPalindrome('racecar');
-checkPalindrome('hello');
-
-const sumDigits = (num) => {
-    const numString = String(num);
-    const digits = numString.split('');
-    let total = 0;
-    for (let i = 0; i < digits.length; i++){
-        total +=(Number(digits[i]));
-    }
-    return total;
-}
-
-console.log(sumDigits(42));
-
-const calculateSide = (sideA, sideB) => {
-    const cSquared = Math.pow(sideA, 2) + Math.pow(sideB, 2);
-    const sideC = Math.sqrt(cSquared);
-    return sideC;
-}
-console.log(calculateSide(3,4));
+console.log(sumArray([1,2,3,4,5]));
 
 

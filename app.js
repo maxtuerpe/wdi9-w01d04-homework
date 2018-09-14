969d89013ec2d1dd0beb0b911f8f92dd6f084c41
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-14 11:12:41 -0600

    Commit 6 - Prime Numbers.

--- app.js
+++ app.js
@@ -3,40 +3,40 @@
 // the function can be used in an expression or inside of other functions
 
 
-// const checkPalindrome = (str) => {
-//     const arr = str.split('');
-//     const reverseArr = arr.reverse();
-//     const reverseStr = reverseArr.join('');
-//     if (str === reverseStr){
-//         console.log(`${str} is a palindrome`);
-//         return true;
-//     } else {
-//         console.log(`${str} is not a palindrome`);
-//         return false;
-//     }
-// }
+const checkPalindrome = (str) => {
+    const arr = str.split('');
+    const reverseArr = arr.reverse();
+    const reverseStr = reverseArr.join('');
+    if (str === reverseStr){
+        console.log(`${str} is a palindrome`);
+        return true;
+    } else {
+        console.log(`${str} is not a palindrome`);
+        return false;
+    }
+}
 
-// checkPalindrome('racecar');
-// checkPalindrome('hello');
+checkPalindrome('racecar');
+checkPalindrome('hello');
 
-// const sumDigits = (num) => {
-//     const numString = String(num);
-//     const digits = numString.split('');
-//     let total = 0;
-//     for (let i = 0; i < digits.length; i++){
-//         total +=(Number(digits[i]));
-//     }
-//     return total;
-// }
+const sumDigits = (num) => {
+    const numString = String(num);
+    const digits = numString.split('');
+    let total = 0;
+    for (let i = 0; i < digits.length; i++){
+        total +=(Number(digits[i]));
+    }
+    return total;
+}
 
-// console.log(sumDigits(42));
+console.log(sumDigits(42));
 
-// const calculateSide = (sideA, sideB) => {
-//     const cSquared = Math.pow(sideA, 2) + Math.pow(sideB, 2);
-//     const sideC = Math.sqrt(cSquared);
-//     return sideC;
-// }
-// console.log(calculateSide(3,4));
+const calculateSide = (sideA, sideB) => {
+    const cSquared = Math.pow(sideA, 2) + Math.pow(sideB, 2);
+    const sideC = Math.sqrt(cSquared);
+    return sideC;
+}
+console.log(calculateSide(3,4));
 
 const sumArray = (arr) => {
     let sum = 0;
@@ -47,5 +47,25 @@ const sumArray = (arr) => {
 }
 
 console.log(sumArray([1,2,3,4,5]));
+
+const checkPrime = (num) => {
+    for ( let i = 2; i <= Math.sqrt(num); i++){
+        if (num%i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(checkPrime(9));
+
+const PrintPrimes = (num) => {
+    for( let i = 1; i <= num; i ++){
+        if(checkPrime(i)){
+            console.log(i);
+        }
+    }
+}
+
+PrintPrimes(97);
 
 
